a09e7ae4426afb2875582edd5477d763ee3d74de
Nikhil <nikhil@Nikhil-PC.(none)>
2014-01-20 23:47:27 -0800

    RuleChecking function returns 'action String' only, not the entire 'Rule object'

--- src/MessagePasser.java
+++ src/MessagePasser.java
@@ -32,38 +32,39 @@ public class MessagePasser {
     @param sendOrReceive: 1 -> check message with ReceiveRules, 0 -> check message with SendRules
     @return: The matched rule object is returned so that "Action name" can be used to decide what to do in the Send() function of MessagePasser
 */
-	public Rule RuleChecking(Message message, int sendOrReceive)
-	   {
-	           ArrayList<Rule> rl = null;
-	           if(sendOrReceive == 0)
-	                   rl = parse.getSendRules();
-	           else if(sendOrReceive == 1)
-	                   rl = parse.getReceiveRules();
-	           else
-	           {
-	                   System.err.println("Wrong rule type given: " + sendOrReceive);
-	                   System.exit(1);
-	           }
-	           for(Rule rule: rl)
-	           {
-	                   if((rule.getSource() != null) && !(rule.getSource().equals(message.getSource())))
-	                           continue;//not match, check next rule
-	                   else if((rule.getDestination() != null) && !(rule.getDestination().equals(message.getDestination())))
-	                           continue;
-	                   else if((rule.getKind() != null) && !(rule.getKind().equals(message.getKind())))
-	                           continue;
-	                   else if( (rule.getId() > 0) && (rule.getId() != message.getId()) )
-	                           continue;
-	                   rule.setComparison();
-	                   if((rule.getNth() > 0) && (rule.getNth() != rule.getComparison()) )
-	                           continue;
-	                   else if( (rule.getEveryNth() > 0) && (rule.getComparison() % rule.getEveryNth()) != 0)
-	                           continue;
-	                   /* Rule matched, so return it */
-	                   return rule;
-	           }
-	           /* If rule not matched, return null */
-	           return null; 
-	   }
-
+	public String RuleChecking(Message message, int sendOrReceive)
+   {
+           ArrayList<Rule> rl = null;
+           if(sendOrReceive == 0)
+                   rl = sendRules;
+           else if(sendOrReceive == 1)
+                   rl = receiveRules;
+           else
+           {
+                   System.err.println("Wrong rule type given: " + sendOrReceive);
+                   System.exit(1);
+           }
+           for(Rule rule: rl)
+           {
+                   if((rule.getSource() != null) && !(rule.getSource().equals(message.getSource()  )))
+                           continue;//not match, check next rule
+                   else if((rule.getDestination() != null) && !(rule.getDestination().equals(message.getDestination())))
+                           continue;
+                   else if((rule.getKind() != null) && !(rule.getKind().equals(message.getKind())))
+                           continue;
+                   /*
+                   else if( (rule.getId() > 0) && (rule.getId() != message.getId()) )
+                           continue;
+                   */
+                   rule.setComparison();
+                   if((rule.getNth() > 0) && (rule.getNth() != rule.getComparison()) )
+                           continue;
+                   else if( (rule.getEveryNth() > 0) && (rule.getComparison() % rule.getEveryNth()) != 0)
+                           continue;
+                   /* Rule matched, so return it*/
+                   return rule.getAction() ;
+           }
+           /* If rule not matched, return null */
+           return null; 
+   }
 }
