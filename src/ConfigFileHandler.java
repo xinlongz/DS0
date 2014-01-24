95babd622bb6cf84e9a61a6f153631ef21cca208
Nikhil <nikhil@Nikhil-PC.(none)>
2014-01-23 23:37:09 -0800

    Please read my comments here and the commented code

--- src/ConfigFileHandler.java
+++ src/ConfigFileHandler.java
@@ -1,4 +1,3 @@
-import java.util.concurrent.*;
 import java.io.File;
 
 public class ConfigFileHandler extends Thread {
@@ -22,10 +21,31 @@ public class ConfigFileHandler extends Thread {
                     if(nowModified != initialTime)
                     {
                     	initialTime = nowModified;
-                    	/** I think we need to stop the current Receiver1 thread also */
+                    	
+                    	/** I think we need to stop the current Receiver1 thread also . Something like this:
+                    	Code in this java file:
+                    	receiver.setFlag();
+                    	receiver.interrupt();
+                    	
+                    	Code in Receiver1 class inside MessagePasser.java:
+                    	public void setFlag(){flag = false;}
+                    	
+                    	and
+                    	
+                    	Inside that infinite loop: 
+                    	while(true) {
+                    		 if(!flag)
+                             	break;
+                    	}
+                    	
+                    	Please check this add the changes
+                    	
+                    	*/
                     	
                     	/* Read the config file again, start the server & receiver threads */
-                    	MessagePasser m = new MessagePasser(configFile, process);
+						MessagePasser m = new MessagePasser(configFile, process);
+						/** This object "m" has to be passed into Initiate.java to get used */
+						
                     }
                     else
                     {
